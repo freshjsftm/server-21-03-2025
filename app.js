4fff21573b8a6857744af8f83dd97edcede57c91
Olena Zdanovska <freshjs.ftm@gmail.com>
2025-03-24 18:22:32 +0200

    Request

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.end('hi from express');
+app.get('/users/:userId', (req, res) => {
+  res.end(`<h1> ${req.path} <br> ${req.params.userId} </h1>`)
 });
 
-app.get('/about', (req, res) => {
-  res.end('page about');
-});
+app.get('/products/:id', (req, res) => {
+  const requestStr = `method: ${req.method},<br>
+   url: ${req.url}, <br>
+   path: ${req.path},<br>
+   query: ${JSON.stringify(req.query)}, <br>   
+   params: ${JSON.stringify(req.params)},<br>
+   key: ${req.params.id},<br>   
+   hostname: ${req.hostname},<br>
+   ip: ${req.ip}`;
 
-// new req   /contact
-
-
-
-/*------------*/
-app.use((req, res) => {
-  res.end('error 404');
+  res.end(`<h1>${requestStr}</h1>`);
 });
 
 module.exports = app;
