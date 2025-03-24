18885495d0a8514953adb3470b145fad6b89dce8
Olena Zdanovska <freshjs.ftm@gmail.com>
2025-03-24 19:01:19 +0200

    Middleware

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.get('/users/:userId', (req, res) => {
-  res.end(`<h1> ${req.path} <br> ${req.params.userId} </h1>`)
-});
-
-app.get('/products/:id', (req, res) => {
-  const requestStr = `method: ${req.method},<br>
-   url: ${req.url}, <br>
-   path: ${req.path},<br>
-   query: ${JSON.stringify(req.query)}, <br>   
-   params: ${JSON.stringify(req.params)},<br>
-   key: ${req.params.id},<br>   
-   hostname: ${req.hostname},<br>
-   ip: ${req.ip}`;
-
-  res.end(`<h1>${requestStr}</h1>`);
-});
+app.get(
+  '/',
+  (req, res, next) => {
+    req.test = 5;
+    console.log('first');
+    next();
+  },
+  (req, res, next) => {
+    console.log('second');
+    req.test *= 4;
+    if (true) {
+      next();
+    }else{
+      res.send('stop');
+    }
+  },
+  (req, res, next) => {
+    console.log('third');
+    res.status(200).send('finish, '+ req.test);
+  }
+);
 
 module.exports = app;
