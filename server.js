47595dbe84e834e3e16a775bfe57ae7f381b6702
Olena Zdanovska <freshjs.ftm@gmail.com>
2025-03-26 17:45:58 +0200

    Add connect to mongodb

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const http = require('http');
-const app = require('./app');
+const http = require('http'); // core modules
+const mongoose = require('mongoose'); // npm modules
+const app = require('./app'); // local own modules
+// localhost  127.0.0.1
+mongoose
+  .connect('mongodb://127.0.0.1:27017/mongo_4')
+  .then(() => {
+    console.log('mongodb connect success');
+  })
+  .catch((err) => {
+    console.log('mongodb connect error: ', err);
+  });
 
 const server = http.createServer(app);
-
 const port = 3000;
 
 server.listen(port, () => {
